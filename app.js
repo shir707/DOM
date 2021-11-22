9a51023037de845893b39419b2798cd13d26c677
Shir Fogel <shirfogel@cs.colman.ac.il>
2021-11-22 10:14:38 +0200

    adding varaiable to block the buttons after winning

--- app.js
+++ app.js
@@ -6,55 +6,57 @@ GAME RULES:
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 */
-var scores,roundScore,activePlayer;
+var scores,roundScore,activePlayer,gamePlaying;
 init();
 
 
 //we want to catch when the user click on the roll button
 document.querySelector('.btn-roll').addEventListener('click', function(){
-    //1.Random number
+    if(gamePlaying){
+        //1.Random number
     //the random give me number between 0 to 1, i want from 1 to 6
     var dice=Math.floor(Math.random()*6)+1;
 
-     //2.Display the result
-     var diceDOM = document.querySelector('.dice');
-     diceDOM.style.display='block';
-     diceDOM.src = 'dice-' + dice + '.png';
+    //2.Display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display='block';
+    diceDOM.src = 'dice-' + dice + '.png';
 
-     //3.Update the round score if the rolled number is not one
-     if(dice!==1){
-         //Add score
-         roundScore+=dice;
-         document.querySelector('#current-' + activePlayer).textContent=roundScore;
-     }
-     else{
-         //Next player and reset the score
+    //3.Update the round score if the rolled number is not one
+    if(dice!==1){
+        //Add score
+        roundScore+=dice;
+        document.querySelector('#current-' + activePlayer).textContent=roundScore;
+    }
+    else{
+        //Next player and reset the score
 
-        nextPlayer();
+       nextPlayer();
 
-         activePlayer === 0 ? activePlayer=1 : activePlayer=0;
-         roundScore=0;
-         document.querySelector('#current-0').textContent=0;
-         document.querySelector('#current-1').textContent=0;
-         //we need to move the dot that says who is play now. in the html is the active next to the class
-         //the toggle is like to do remove and after add to the "active"
-         document.querySelector('.player-0-panel').classList.toggle('active');
-         document.querySelector('.player-1-panel').classList.toggle('active');
+        activePlayer === 0 ? activePlayer=1 : activePlayer=0;
+        roundScore=0;
+        document.querySelector('#current-0').textContent=0;
+        document.querySelector('#current-1').textContent=0;
+        //we need to move the dot that says who is play now. in the html is the active next to the class
+        //the toggle is like to do remove and after add to the "active"
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
 
-         document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.dice').style.display = 'none';
 
-     }
+    }
+
+    }
+    
 
 });
 
 document.querySelector('.btn-hold').addEventListener('click',function(){
-    //1.add the current score to the global score
+    if(gamePlaying){
+        //1.add the current score to the global score
     scores[activePlayer]+=roundScore;
-
     //2. update UI
     document.querySelector('#score-' + activePlayer).textContent=scores[activePlayer];
-
-
     //3.check if the player won the game
     if(scores[activePlayer] >=20){
         document.querySelector('#name-' + activePlayer).textContent='Winner!';
@@ -62,12 +64,15 @@ document.querySelector('.btn-hold').addEventListener('click',function(){
         //giving the display of the winner
         document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
         document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+        gamePlaying=false;
     }
     else{
         nextPlayer();
 
     }
 
+    }
+    
 });
 
 function nextPlayer(){
@@ -85,6 +90,7 @@ function nextPlayer(){
 }
 
 function init(){
+    gamePlaying=true;
     scores=[0,0];
     activePlayer=0;
     roundScore=0;
